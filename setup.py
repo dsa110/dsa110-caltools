05d583dafe23a9dda3e32dbe89132d28b09e2a67
Casey Law <caseyjlaw@gmail.com>
2019-10-07 16:56:21 -0700

    basic functions working and setuptools working

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsa110-caltools',
+      version='0.1',
+      url='http://github.com/dsa110/dsa110-caltools',
+      packages=['caltools'],
+      zip_safe=False)
