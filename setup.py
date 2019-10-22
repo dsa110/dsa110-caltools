1bbabb5be302ce3a79b4721c1b4e67290d4baf95
Casey Law <caseyjlaw@gmail.com>
2019-10-22 15:46:07 -0700

    adding future dep

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='dsa110-caltools',
       version='0.2',
       url='http://github.com/dsa110/dsa110-caltools',
       packages=['caltools'],
-      requirements=['astropy', 'astroquery', 'numpy'],
+      requirements=['astropy', 'astroquery', 'numpy', 'future'],
       zip_safe=False)
