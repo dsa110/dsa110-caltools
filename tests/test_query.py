d84bc3db97a6db7900a0c84a1e97e57756b459f5
Casey Law <caseyjlaw@gmail.com>
2019-10-23 09:23:08 -0700

    fixing tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -5,7 +5,7 @@ from caltools import caltools
 def test_querycoord():
     from astropy import coordinates as co, units as u
     coord = co.SkyCoord(ra=180., dec=0., unit=(u.deg, u.deg))
-    table = caltools.query_heasarc(coord=coord, surveys=['NVSS'])
+    table = caltools.query_heasarc(coord=coord, mission='NVSS')
     print(table)
     assert table is not None
 
