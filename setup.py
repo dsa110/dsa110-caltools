9eebe82a481d5de2903b4827e17efcc0b0688a9f
Dana Simard <dana.simard@astro.caltech.edu>
2021-09-23 22:45:39 +0000

    renaming of repository

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(name='dsa110-caltools',
+setup(name='dsa110-catalogs',
       version='0.2',
-      url='http://github.com/dsa110/dsa110-caltools',
-      packages=['caltools'],
+      url='http://github.com/dsa110/dsa110-catalogs',
+      packages=['catalogs'],
       requirements=['astropy', 'astroquery', 'numpy'],
       zip_safe=False)
